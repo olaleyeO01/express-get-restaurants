cbfc3948b9e7a5f44af1f5bd49e11b8fbd634feb
Olaleye Oduola <olaoduola01@gmail.com>
2023-08-07 15:58:32 +0100

    data visibily showing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
-
-
-
+//TODO: Create your GET Request Route Below:
+app.get("/restaurants", async (request, response) => {
+  const restaurants = await Restaurant.findAll();
+  response.json(restaurants);
+});
 
 module.exports = app;
